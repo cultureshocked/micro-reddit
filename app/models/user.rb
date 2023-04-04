c13c34010943142efca9365f4e76311976ef5c6a
Conrad Fitzgerald <cfitz012@uottawa.ca>
2023-04-04 08:59:56 -0400

    Generate ActiveRecord models; Add model validations

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_many :posts
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+end
